70dfac9938cb469a87e15b022c3adde0368990ca
AlexeySafronov <alexey.safronov@onlyoffice.com>
2021-09-20 16:59:55 +0300

    Web: API: Added cdnUrl for module's images

--- packages/asc-web-common/api/modules/index.js
+++ packages/asc-web-common/api/modules/index.js
@@ -6,16 +6,16 @@ export function getModulesList() {
     method: "get",
     url: "/modules/info",
   }).then((modules) => {
-    const workingModules = modules.filter(
-      (module) => typeof module === "object"
-    );
-    const newModules = workingModules.map((m) => {
-      return {
-        ...m,
-        iconUrl: combineUrl(m.link, m.iconUrl),
-        imageUrl: combineUrl(m.link, m.imageUrl),
-      };
-    });
+    const publicPath = window.AppServer?.cdnUrl || "";
+    const newModules = modules
+      .filter((module) => typeof module === "object")
+      .map((m) => {
+        return {
+          ...m,
+          iconUrl: combineUrl(publicPath, m.link, m.iconUrl),
+          imageUrl: combineUrl(publicPath, m.link, m.imageUrl),
+        };
+      });
 
     return newModules;
   });

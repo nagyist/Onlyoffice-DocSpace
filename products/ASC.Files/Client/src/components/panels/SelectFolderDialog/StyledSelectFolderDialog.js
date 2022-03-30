57bbab93271d41e3e7c8e79e0d5ab9eb485d8279
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2022-03-30 16:00:44 +0300

    Web: Files: selectFolderDialog: Moved styles to separate file.

---
+++ products/ASC.Files/Client/src/components/panels/SelectFolderDialog/StyledSelectFolderDialog.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.div`
+  .dialog-header {
+    display: flex;
+  }
+  .arrow-button {
+    margin: auto 12px auto 0;
+  }
+`;
+
+const StyledRootPage = styled.div`
+  .root-loader {
+    display: flex;
+  }
+`;
+const StyledBody = styled.div`
+  height: 100%;
+  .select-folder_content-body {
+    display: grid;
+    height: calc(100% - 32px);
+    grid-template-rows: max-content auto max-content;
+    .select-dialog_header-child {
+      ${(props) => props.headerChild && `padding-bottom: 16px;`}
+    }
+  }
+  .select-folder_list-body {
+    height: 100%;
+  }
+
+  .select-folder_loader {
+    overflow: hidden;
+    .list-loader-wrapper {
+      padding: 0;
+    }
+  }
+  .select-folder_list-loader {
+    display: flex;
+    div:first-child {
+      margin-right: 8px;
+    }
+  }
+  .select-dialog_footer {
+    border-top: 1px solid #eceef1;
+    padding-top: 16px;
+    .select-dialog_buttons {
+      display: flex;
+      padding: 0 16px 0 16px;
+
+      margin-left: -16px;
+      margin-right: -16px;
+      width: 100%;
+      box-sizing: border-box;
+      button:first-child {
+        margin-right: 10px;
+      }
+    }
+    .select-dialog_footer-child {
+      ${(props) => props.footerChild && `padding-bottom: 16px;`}
+    }
+  }
+`;
+
+export { StyledRootPage, StyledBody, StyledHeader };
--- products/ASC.Files/Client/src/components/panels/SelectFolderDialog/index.js
+++ products/ASC.Files/Client/src/components/panels/SelectFolderDialog/index.js
@@ -17,71 +17,15 @@ import {
 import RootPage from "./SubComponents/RootPage";
 import ElementsPage from "./SubComponents/ElementsPage";
 import IconButton from "@appserver/components/icon-button";
-import styled from "styled-components";
 import Button from "@appserver/components/button";
 import Loaders from "@appserver/common/components/Loaders";
 import { FolderType } from "@appserver/common/constants";
+import {
+  StyledRootPage,
+  StyledBody,
+  StyledHeader,
+} from "./StyledSelectFolderDialog";
 
-const StyledHeader = styled.div`
-  .dialog-header {
-    display: flex;
-  }
-  .arrow-button {
-    margin: auto 12px auto 0;
-  }
-`;
-
-const StyledRootPage = styled.div`
-  .root-loader {
-    display: flex;
-  }
-`;
-const StyledBody = styled.div`
-  height: 100%;
-  .select-folder_content-body {
-    display: grid;
-    height: calc(100% - 32px);
-    grid-template-rows: max-content auto max-content;
-    .select-dialog_header-child {
-      ${(props) => props.headerChild && `padding-bottom: 16px;`}
-    }
-  }
-  .select-folder_list-body {
-    height: 100%;
-  }
-
-  .select-folder_loader {
-    overflow: hidden;
-    .list-loader-wrapper {
-      padding: 0;
-    }
-  }
-  .select-folder_list-loader {
-    display: flex;
-    div:first-child {
-      margin-right: 8px;
-    }
-  }
-  .select-dialog_footer {
-    border-top: 1px solid #eceef1;
-    padding-top: 16px;
-    .select-dialog_buttons {
-      display: flex;
-      padding: 0 16px 0 16px;
-
-      margin-left: -16px;
-      margin-right: -16px;
-      width: 100%;
-      box-sizing: border-box;
-      button:first-child {
-        margin-right: 10px;
-      }
-    }
-    .select-dialog_footer-child {
-      ${(props) => props.footerChild && `padding-bottom: 16px;`}
-    }
-  }
-`;
 class SelectFolderDialog extends React.PureComponent {
   constructor(props) {
     super(props);

96f419a7d1486ea9c2ef1d20562d02e09f60b3e8
Timofey Boyko <55255132+TimofeyBoyko@users.noreply.github.com>
2023-02-08 17:40:30 +0300

    Web:Editor: add type: module to package.json and fix buildTranslatons

--- packages/editor/scripts/buildTranslations.js
+++ packages/editor/scripts/buildTranslations.js
@@ -1,5 +1,9 @@
-const path = require("path");
-const beforeBuild = require("@docspace/common/utils/beforeBuild");
+import path from "path";
+import beforeBuild from "../../common/utils/beforeBuild.js";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 beforeBuild(
   [
